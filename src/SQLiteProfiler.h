1610377f4297b9e08368ca27a4d018136829a9c7
Stephen Smith <blackrim@gmail.com>
2011-10-19 23:53:15 -0400

    indents

--- src/SQLiteProfiler.h
+++ src/SQLiteProfiler.h
@@ -36,52 +36,52 @@ using namespace std;
 #include "libsqlitewrapped.h"
 
 class SQLiteProfiler{
-	private:
-		string gene_name;
-		vector<string> file_names;
-		string cladename;
-		string db;
-		string profilefoldername;
-		bool use_orphan;
-		bool automated;
-		bool updatedb;
-		Tree * userguidetree;
-		bool usertree;
-		void get_children(string in_id, vector<string> * in_ids, vector<string> * in_keepids);
-		vector<string> get_final_children(string id);
-		int count_seqs(string dirc, string file_name);
-		string get_right_one(vector<string> allids,Query & res);
-		vector<string> get_left_right_children(string id);
-		void create_distances(string clade_name, vector<string> names,map<string,string> * numnames,
-				map<string,string>* namesnum, vector< vector<double> > * numlist);
-		void create_distances_user_tree(vector<string> names,map<string,string> * numnames
-				,map<string,string> * namesnum, vector< vector<double> > * numlist);
-		void get_shortest_distance_with_dicts(vector<string> names,map<string,string> numnames,map<string,string> namesnum,
-				vector< vector<double> > numlist, string * shortestnameone, vector<string> * shortestnametwo);
-		void clean_before_profile(string infile);
-		void profile(map<string,string> numnames,map<string,string> namesnum,
-						vector< vector<double> > numlist);
-		void copy_final_file(string filename);
-		string get_name_from_tax_id(string taxid);
-		void calculate_for_removal(vector<Sequence> * seqs,
-				map<string,double> & allmeans);
-		void calculate_for_removal_quicktree(vector<Sequence> * seqs,
-						map<string,double> & allmeans);
-		void remove_outliers();
-		void rename_final_alignment(string which);
+private:
+    string gene_name;
+    vector<string> file_names;
+    string cladename;
+    string db;
+    string profilefoldername;
+    bool use_orphan;
+    bool automated;
+    bool updatedb;
+    Tree * userguidetree;
+    bool usertree;
+    void get_children(string in_id, vector<string> * in_ids, vector<string> * in_keepids);
+    vector<string> get_final_children(string id);
+    int count_seqs(string dirc, string file_name);
+    string get_right_one(vector<string> allids,Query & res);
+    vector<string> get_left_right_children(string id);
+    void create_distances(string clade_name, vector<string> names,map<string,string> * numnames,
+			  map<string,string>* namesnum, vector< vector<double> > * numlist);
+    void create_distances_user_tree(vector<string> names,map<string,string> * numnames
+				    ,map<string,string> * namesnum, vector< vector<double> > * numlist);
+    void get_shortest_distance_with_dicts(vector<string> names,map<string,string> numnames,map<string,string> namesnum,
+					  vector< vector<double> > numlist, string * shortestnameone, vector<string> * shortestnametwo);
+    void clean_before_profile(string infile);
+    void profile(map<string,string> numnames,map<string,string> namesnum,
+		 vector< vector<double> > numlist);
+    void copy_final_file(string filename);
+    string get_name_from_tax_id(string taxid);
+    void calculate_for_removal(vector<Sequence> * seqs,
+			       map<string,double> & allmeans);
+    void calculate_for_removal_quicktree(vector<Sequence> * seqs,
+					 map<string,double> & allmeans);
+    void remove_outliers();
+    void rename_final_alignment(string which);
 
-		//updated runs things
-		vector<string> updatedfiles;
-		vector<string> profilerun;
-		map<string,vector<string> > profilekey;
-		vector<string> flaggedprofiles; // these are the ones that need to be rerun
-		void update_profile();
-		string get_profilekey_value(string profile_string);
-	public:
-		SQLiteProfiler(string gn, string cn, string dbs, bool autom,bool updb);
-		void prelimalign();
-		void run();
-		void set_user_guide_tree(Tree * tree);
+    //updated runs things
+    vector<string> updatedfiles;
+    vector<string> profilerun;
+    map<string,vector<string> > profilekey;
+    vector<string> flaggedprofiles; // these are the ones that need to be rerun
+    void update_profile();
+    string get_profilekey_value(string profile_string);
+public:
+    SQLiteProfiler(string gn, string cn, string dbs, bool autom,bool updb);
+    void prelimalign();
+    void run();
+    void set_user_guide_tree(Tree * tree);
 };
 
 #endif /* MQPROFILER_H_ */
